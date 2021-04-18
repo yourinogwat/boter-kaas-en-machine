0ed9961fe7941c3b5c315f8c460d2b486acf3bf8
yourinogwat <45482503+yourinogwat@users.noreply.github.com>
2021-04-18 12:02:42 +0200

    Add files via upload

--- opdr Machine learning boter.py
+++ opdr Machine learning boter.py
@@ -1,7 +1,7 @@
 from bke import MLAgent, is_winner, opponent, start
 
 class MyAgent(MLAgent):
-    def evaluate(seld, board):
+    def evaluate(self, board):
         if is_winner(board, self.symbol):
             reward = 1
         elif is_winner(board, opponent[self.symbol]):
@@ -9,3 +9,6 @@ class MyAgent(MLAgent):
         else:
             reward = 0
         return reward
+    
+actualGod = MyAgent()
+start(player_x = actualGod)
